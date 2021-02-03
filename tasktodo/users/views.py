74efd98c62e57bb08e85ca494040bbd490140cdd
luizjuniordeveloper <luizjuniordeveloper@gmail.com>
2021-02-02 21:44:21 -0300

    update - add permissions /users/ & /users/id

--- tasktodo/users/views.py
+++ tasktodo/users/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import viewsets
 from rest_framework.views import APIView
-#from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from django.contrib.auth.models import User
 
@@ -16,13 +16,13 @@ from django.http import Http404
 
 class UserList(generics.ListAPIView):
     name = 'users-list'
-    #permission_classes = (IsAdminUser)
+    permission_classes = (IsAdminUser)
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 
 class UserDetail(generics.RetrieveAPIView):
     name = 'user-detail'
-    #permission_classes = (IsAuthenticated)
+    permission_classes = (IsAuthenticated)
     serializer_class = UserSerializer
     queryset = User.objects.all()
