15023ce623c8797deb7d3ef1137a5f9cd47d6d5e
luizjuniordeveloper <luizjuniordeveloper@gmail.com>
2021-02-02 21:14:38 -0300

    create - /user/ & /users/:id [GET]

--- tasktodo/users/serializers.py
+++ tasktodo/users/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
 from django.contrib.auth.models import User
 
-
 class UserSerializer(HyperlinkedModelSerializer):
-    class Meta:
-        Model = User
-        fields = ('username', 'email', 'password', 'first_name', 'last_name')
-        serializer_class 
+    class Meta:         
+        model = User
+        fields = ('url','username', 'first_name', 'last_name', 'email')  
--- tasktodo/users/views.py
+++ tasktodo/users/views.py
@@ -1,17 +1,28 @@
 
-from rest_framework import generics, permissions
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import viewsets
+from rest_framework.views import APIView
+#from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from django.contrib.auth.models import User
+
+from users.serializers import UserSerializer
+
+from django.http import Http404
 
 
 
 class UserList(generics.ListAPIView):
     name = 'users-list'
-    permission_classes = [permissions.is_superuser]
-    queryset  = User.objects.all()
+    #permission_classes = (IsAdminUser)
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
 
 
-class UserDetail(generics.RetrieveDestroyAPIView):
-    name = 'users-detail'
-    permission_classes = [permissions.is_authenticated]
-    queryset  = User.objects.all()
+class UserDetail(generics.RetrieveAPIView):
+    name = 'user-detail'
+    #permission_classes = (IsAuthenticated)
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
