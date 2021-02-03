e9009f26a8eddb703e0667159bc9d85726d67f89
luizjuniordeveloper <luizjuniordeveloper@gmail.com>
2021-02-02 21:41:10 -0300

    add - jwt

--- tasktodo/tasktodo/settings.py
+++ tasktodo/tasktodo/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
     ),
 }
 
--- tasktodo/users/urls.py
+++ tasktodo/users/urls.py
@@ -3,36 +3,22 @@ from django.urls import path, re_path
 
 from users.views import UserList, UserDetail
 
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="TaskToDo API",
-      default_version='v2',
-      description="API ToDo List",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="luizjuniordeveloper@gmail.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+# JWT
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
 )
+
 
 urlpatterns = [
     
+   path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+   path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
- 
-    path('users/', UserList.as_view(), name=UserList.name),
-    path('users/<int:pk>/', UserDetail.as_view(), name=UserDetail.name),
+   path('api/users/', UserList.as_view(), name=UserList.name),
+   path('api/users/<int:pk>/', UserDetail.as_view(), name=UserDetail.name),
 
 
 
 ]
  
- 
- 
-    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
